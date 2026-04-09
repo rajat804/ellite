2a7a2086f66d1919375682fa8e4fc795a1829ae0
Rajat <rajat804@gmail.com>
2026-04-09 17:08:18 +0530

    app js update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ServicesPage from "./pages/ServicesPage";
+import ScrollToTop from "./components/ScrollToTop";
+import AboutPage from "./pages/AboutPage";
+import GalleryPage from "./pages/GalleryPage";
+import ContactPage from "./pages/ContactPage";
+import VisionPage from "./pages/VisionPage";
+import TermsPage from "./pages/TermsPage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 
 const App = () => {
   return (
-    <>  
-        <Header />
-          <Home />
-        <Footer />
-    </>
-  )
-}
+    <>
+      <Router>
+        <ScrollToTop />
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/vision" element={<VisionPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/privacy" element={<PrivacyPolicyPage />} />
 
-export default App
+
+
+        </Routes>
+      <Footer />
+      </Router>
+
+    </>
+  );
+};
+
+export default App;
