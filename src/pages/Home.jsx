9df142d90d1e835a1a4a75913564178e0d644549
Rajat <rajat804@gmail.com>
2026-04-18 19:03:45 +0530

    home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import HeroSection from '../components/HeroSection'
 import HowItWorks from '../components/HowItWorks'
 import WhyChooseUs from '../components/WhyChooseUs'
 import PetServices from '../components/PetServices'
+import BoardingFeatures from '../components/BoardingFeatures'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <HowItWorks />
+      <BoardingFeatures />
       <WhyChooseUs />
       <PetServices />
     </>
