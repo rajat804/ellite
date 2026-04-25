ebf5cb091a6342b5dcb5e064566cc144bc3dc0de
Rajat <rajat804@gmail.com>
2026-04-25 11:47:15 +0530

    assets update

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import img1 from './IMG_1.jpeg';
+import img2 from './img2.jpg';
+import img3 from './IMG_3.jpg';
+import img4 from './IMG_4.jpg';
+import img5 from './IMG_5.jpg';
+
+
+
+export const assets = {
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+}
