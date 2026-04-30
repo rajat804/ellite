c92a95bafbf0d93a9ca19f987c6e6fe5ba7b6ee9
Rajat <rajat804@gmail.com>
2026-04-30 16:14:56 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
             <div className="flex items-center gap-6">
               <div className="flex items-center gap-2 group cursor-pointer">
                 <FaPhone className="text-[#D4A054] group-hover:scale-110 transition-transform" />
-                <span>Emergency: +91-9582582689</span>
+                <span>Emergency: +91-9971200705</span>
               </div>
               <div className="flex items-center gap-2 group cursor-pointer">
                 <FaHeart className="text-[#D4A054] group-hover:scale-110 transition-transform" />
