d429b1a563454c047e5ccd4cf2d1c900f68d397a
Rajat <rajat804@gmail.com>
2026-05-08 15:13:41 +0530

    assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,6 +3,25 @@ import img2 from './img2.jpg';
 import img3 from './IMG_3.jpg';
 import img4 from './IMG_4.jpg';
 import img5 from './IMG_5.jpg';
+import dogInsideImg from './dog-inside-img.png';
+import dogSalonOne from './dog-salon-1.png';
+import dogSalonTwo from './dog-salon-2.png';
+import dogSalonThree from './dog-salon-3.png';
+import gateOne from './gate-1.png';
+import gateTwo from './gate-2.png';
+import gateThree from './gate-3.png';
+import gateFour from './gate-4.png';
+import outsideOne from './outside-1.png';
+import posterImgOne from './poster-img-1.png';
+import posterImgTwo from './poster-img-2.png';
+import posterImgThree from './poster-img-3.png';
+import posterImgFour from './poster-img-4.png';
+import swimmingOne from './swimming-1.png';
+import swimmingTwo from './swimming-2.png';
+import swimmingThree from './swimming-3.png';
+import swimmingFour from './swimming-4.png';
+import videoOne from './video-one.mp4';
+import videoTwo from './video-two.mp4';
 
 
 
@@ -12,4 +31,7 @@ export const assets = {
     img3,
     img4,
     img5,
+    dogInsideImg,
+    dogSalonOne,dogSalonTwo,dogSalonThree,gateOne,gateTwo,gateThree,gateFour,outsideOne,posterImgOne,posterImgTwo,posterImgThree,posterImgFour,
+    swimmingOne,swimmingTwo,swimmingThree,swimmingFour,videoOne,videoTwo
 }
